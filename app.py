1512d3e1345a36941de8357cb6765609a8c1b90c
connorwalsh <c@polygon.pizza>
2018-05-08 20:29:34 -0400

    setting up db

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-# flask_web/app.py
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///users.sqlite3'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+def index():
+    return render_template(
+        'index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # authenticate the user and stuff...
+        print(username)
+        print(password)
+
+        return ''
+    elif request.method == 'GET':
+        return render_template(
+            'login.html')
+
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template(
+        'profile.html', name=name)
+
+
+@app.route("/products")
+def products(name):
+    return render_template(
+        'test.html', name=name)
 
 
 if __name__ == '__main__':
